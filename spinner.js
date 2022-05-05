ba254f33a8c5d72e267418dbdd34031289037570
Cesar Burgueño Victorio <cbvictorio@gmail.com>
2022-05-05 12:58:48 -0700

    modified spinner

--- spinner.js
+++ spinner.js
@@ -3,13 +3,9 @@ const { Spinner } = require('cli-spinner')
 // amount of seconds times 1000
 const SPINNER_LIMIT = 5 * 1000
 
-const huskyMessage = '[Husky] -> Verifying coding format...'
+const huskyMessage = '[Husky] -> Verifying coding format...\n'
 const spinnerProgress = '⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏'
 
 const spinner = new Spinner(huskyMessage)
 spinner.setSpinnerString(spinnerProgress)
 spinner.start()
-
-setTimeout(() => {
-    spinner.stop()
-}, SPINNER_LIMIT)
