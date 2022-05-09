16e8591508a647849cd41767c64b0bef2df97c20
Cesar Burgueño Victorio <cbvictorio@gmail.com>
2022-05-09 10:01:48 -0700

    modified pokemon page structure to have a better reading

--- pages/pokemon/[slug].tsx
+++ pages/pokemon/[slug].tsx
@@ -7,21 +7,6 @@ type IProps = {
     slug?: PokemonSlug
 }
 
-/* 
-    getServerSideProps and getStaticProps can only be used on PAGES file, 
-    it cannot be used anywhere else 
-*/
-
-export const getServerSideProps = async (ctx: GetServerSidePropsCtx) => {
-    const {
-        query: { slug },
-    } = ctx
-    const pokemonData: PokemonFetchResponse = await PokemonAPI.getPokemonByNameOrId(slug)
-    return {
-        props: { pokemonData, slug },
-    }
-}
-
 const Product: FC<IProps> = ({ pokemonData, slug }: IProps) => {
     const { id, name } = pokemonData as PokemonData
     const { error, message } = pokemonData as GenericError
@@ -41,6 +26,21 @@ const Product: FC<IProps> = ({ pokemonData, slug }: IProps) => {
             <h2> Pokemon Name: {name} </h2>
         </div>
     )
+}
+
+/* 
+    getServerSideProps and getStaticProps can only be used on PAGES file, 
+    it cannot be used anywhere else 
+*/
+
+export const getServerSideProps = async (ctx: GetServerSidePropsCtx) => {
+    const {
+        query: { slug },
+    } = ctx
+    const pokemonData: PokemonFetchResponse = await PokemonAPI.getPokemonByNameOrId(slug)
+    return {
+        props: { pokemonData, slug },
+    }
 }
 
 export default Product
