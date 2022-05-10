49c75ca8fe3a268cd27345d5fdb54ddf36ffd6a8
Cesar Burgueño Victorio <cbvictorio@gmail.com>
2022-05-10 08:56:39 -0700

    added some comments to understand the difference between SSG and SSR

--- pages/pokemon/[slug].tsx
+++ pages/pokemon/[slug].tsx
@@ -1,5 +1,4 @@
 import { FC } from 'react'
-import { useRouter } from 'next/router'
 import PokemonAPI from '@/db'
 
 type IProps = {
@@ -30,7 +29,22 @@ const Product: FC<IProps> = ({ pokemonData, slug }: IProps) => {
 
 /* 
     getServerSideProps and getStaticProps can only be used on PAGES file, 
-    it cannot be used anywhere else 
+    it cannot be used anywhere else.
+
+    * NOTE: Difference between Static Site Generation (SSG) and Server Side Rendering (SSR)
+
+    SSR: Any request on the server side is known as 'On-Demand', which means that
+         every time an user opens a page, a call to the API will be performed every single
+         time that page is opened up.
+    
+    SSG: The call to the API happens just once, allowing Next JS to build a static page,
+         which means that the html, css and JS files will be previously generated and 
+         ready to be served with the info from the API, with all the markup and all the info
+         previously generated.
+
+    For this particular case, a pokemon page has to be a SSR site, since the id or name
+    of the pokemon is dynamic, we cannot necessarily generate a static page for it.
+
 */
 
 export const getServerSideProps = async (ctx: GetServerSidePropsCtx) => {
@@ -42,5 +56,15 @@ export const getServerSideProps = async (ctx: GetServerSidePropsCtx) => {
         props: { pokemonData, slug },
     }
 }
+
+// export const getStaticProps = async (ctx: GetServerSidePropsCtx) => {
+//     const {
+//         query: { slug },
+//     } = ctx
+//     const pokemonData: PokemonFetchResponse = await PokemonAPI.getPokemonByNameOrId(slug)
+//     return {
+//         props: { pokemonData, slug },
+//     }
+// }
 
 export default Product
